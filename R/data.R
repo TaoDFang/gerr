b9a27ac3abe0a3589e668ed6bbaa9c0370e6ce2f
Tao Fang <talonvonfang@gmail.com>
2019-04-28 16:55:09 +0100

    update on 28/04/2019

---
+++ R/data.R
@@ -0,0 +1,57 @@
+#' Homo sapiens GO ontology tree 
+#'@description A rds R object contains GO ontology relationships (tree structure)
+#'@usage readRDS(system.file("data", "human_go_ontology.rds", package = "GENEMABR"))
+#'@format Directed igraph format
+#'@source http://geneontology.org/docs/download-ontology/
+#'@details  The igraph format tree was constructed by using data from http://geneontology.org/docs/download-ontology/ (May 2108)
+#'It has three root notes representing Molecular Function,Cellular Component and Biological Process (http://geneontology.org/docs/ontology-documentation/)
+"human_go_ontology"
+
+
+#' human_go_roots
+#'@description A rds R object contains GO ontology tree root nodes
+#'@usage  readRDS(system.file("data", "human_go_roots.rds", package = "GENEMABR"))
+#'@format A vector of GO ontology root notes (ID)
+#'@source http://geneontology.org/docs/download-ontology/
+#'@details  You can view tree stuctor of GO ontology at  https://www.ebi.ac.uk/QuickGO/
+#'Thre are three roots notes in GO ontology tree: GO:0008150 (biological_process), GO:0003674(molecular_function), GO:0005575(cellular_component)
+"human_go_roots"
+
+#' human_go_sub_roots
+#'@description A rds R object contains GO ontology tree sub-root nodes (The children of root nodes).
+#'@usage readRDS(system.file("data", "human_go_sub_roots.rds", package = "GENEMABR"))
+#'@format A list of three elements contains GO ontology sub-root notes (ID)/the children of three root notes
+#'@source http://geneontology.org/docs/download-ontology/
+#'@details  You can view tree stuctor of GO ontology at  https://www.ebi.ac.uk/QuickGO/
+"human_go_sub_roots"
+
+
+#' Homo sapiens REACTOME ontology tree 
+#'@description A rds R object contains Reactome ontology relationships (tree structure)
+#'@usage readRDS(system.file("data", "human_reactome_ontology.rds", package = "GENEMABR"))
+#'@format Directed igraph format
+#'@source https://reactome.org/download-data
+#'@details  The igraph format tree was constructed by using  data from https://reactome.org/download-data (May 2108)
+#'It has several root nodes representing REACTOME pathway categories (https://reactome.org/PathwayBrowser/)
+"human_reactome_ontology"
+
+
+#' human_reactome_roots
+#'@description A rds R object contains REACTOME tree root nodes
+#'@usage readRDS(system.file("data", "human_reactome_roots.rds", package = "GENEMABR"))
+#'@format A vector of REACTOME root notes (ID)
+#'@source https://reactome.org/download-data
+#'@details  You can view tree stuctor of REACTOME at https://reactome.org/PathwayBrowser/
+"human_reactome_roots"
+
+
+#' Homo sapiens GO ontology and REACTOME ontology gene-pathway realtionship
+#'@description A rds R object contains GO ontology and REACTOME ontology gene-pathway realtionship
+#'@usage readRDS(system.file("data", "gene_pathway_matrix.rds", package = "GENEMABR"))
+#'@format Formal class 'dgCMatrix' [package "Matrix"] 
+#'@source http://geneontology.org/docs/download-ontology/, https://reactome.org/download-data
+#'A binary matrix whose columns are the pathways/gene sets from GO ontology and REATOME database and 
+#'whose rows are all the genes(represented by gene HUGO gene symbols) from  GO ontology and REATOME database.
+#'For gene i and pathway j, the value of matrix(i,j) is 1 is gene i belonging to pathway j otherwise 0
+"gene_pathway_matrix"
+
---
+++ R/supplementary_functions.R
@@ -0,0 +1,151 @@
+#' fisher_exact_test
+#' 
+#' This function allows you to compute two sided fish exact pvalue of gene list for selected  pathways
+#' To know more about this method. I recommend you to read the paper (Enrichment or depletion of a GO category within a class of genes: which test?) for more details
+#' @param selected_pathways A vecor of pathways to be used for enrichment analysis for genes in \emph{gene_input}.It should have same ID types(E.g. pathway ID, pathway names) as the pathways in \emph{gene_pathway_matrix}.
+#' @param gene_input A vecor of genes to be annotated. It should have same ID types(E.g. Ensembl ID, HUGO gene symbol) as the genes in \emph{gene_pathway_matrix}.
+#' @param gene_pathway_matrix A binary background matrix whose columns are the pathways/gene sets and 
+#'whose rows are all the genes from pathways/gene sets . It could be in sparse matrix format ((inherit from class "sparseMatrix" as in package Matrix) to save memory.
+#'For gene i and pathway j, the value of matrix(i,j) is 1 is gene i belonging to pathway j otherwise 0.
+#'Users could leave it as default value("default") so it will use pre-collected gene_pathway_matrix from GO Ontology and REACTOME databaase.
+#'Otherwise, they could use their own customized gene_pathway_matrix 
+#' @return  A list of two elements: 
+#' \itemize{
+#'   \item selected_pathways_fisher_pvalue - Fisher exact pvalue for selected pathways
+#'   \item selected_pathways_num_genes - The number of genes for selected pathways in background
+#' }
+#' @keywords 
+#' @export
+#' @examples
+#' a=fisher_exact_test(selected_pathways=c("GO:0007250","GO:0008625"),gene_input=c("TRPC4AP","CDC37","TNIP1","IKBKB","NKIRAS2","NFKBIA","TIMM50","RELB","TNFAIP3","NFKBIB","HSPA1A","NFKBIE","SPAG9","NFKB2","ERLIN1","REL","TNIP2","TUBB6","MAP3K8"),gene_pathway_matrix="default")
+fisher_exact_test=function(selected_pathways,gene_input,gene_pathway_matrix="default"){ 
+  if(gene_pathway_matrix=="default"){
+    gene_pathway_matrix=readRDS(system.file("data", "gene_pathway_matrix.rds", package = "GENEMABR"))
+  }
+  all_genes=rownames(gene_pathway_matrix)
+  module1_common_genes=intersect(all_genes,gene_input) 
+  
+  selected_pathways_fisher_pvalue=vector()
+  selected_pathways_num_genes=vector()
+  for(index in 1:length(selected_pathways)){
+    fisher_pathway=selected_pathways[index]
+    #          genes_in_common      genes_in_reference
+    #pathway          a                   c
+    #no_pathway       b                   d
+    a=sum(gene_pathway_matrix[module1_common_genes,fisher_pathway])
+    b=length(module1_common_genes)-a
+    c=sum(gene_pathway_matrix[,fisher_pathway])-a
+    d=length(all_genes)-a-c-b
+    contigency_table=matrix(c(a,b,c,d),nrow = 2)
+    fisher_result=fisher.test(contigency_table)
+    selected_pathways_fisher_pvalue[fisher_pathway]=fisher_result[['p.value']]
+    selected_pathways_num_genes[fisher_pathway]=sum(gene_pathway_matrix[,fisher_pathway])
+  }
+  return(list(selected_pathways_fisher_pvalue=selected_pathways_fisher_pvalue,selected_pathways_num_genes=selected_pathways_num_genes))
+}
+
+
+
+#' find_root_ids
+#' 
+#' If you use the default pathway databases(GO Ontologyand REACTOME),this function allows you to extract GO sub-roots or REACTOME roots for certain pathways from GO or REACTOME
+#' to help you better understanding thier the biological meanings
+#' @param selected_pathways A vecor of GO and/or REACTOME pathways IDs. 
+#' @return  A list of GO sub-root or REACTOME root ids for provided pathways. 
+#' If a certain pathway has morn than one GO sub-roots or REACTOME roots, they will be seperated by "#".
+#' @keywords 
+#' @export
+#' @examples
+#' find_root_ids(selected_pathways=c("GO:0005834","R-HSA-111469"))
+find_root_ids=function(selected_pathways){
+  go_ontology=readRDS(system.file("data", "human_go_ontology.rds", package = "GENEMABR"))
+  go_sub_roots=readRDS(system.file("data", "human_go_sub_roots.rds", package = "GENEMABR"))
+  go_sub_roots=unlist(go_sub_roots)
+  reactome_ontology=readRDS(system.file("data", "human_reactome_ontology.rds", package = "GENEMABR"))
+  reactome_roots=readRDS(system.file("data", "human_reactome_roots.rds", package = "GENEMABR"))
+                            
+  find_roots=lapply(selected_pathways, function(x){
+    if(grepl("GO",x)){
+      parent_nodes=ego(go_ontology,order = gorder(go_ontology),x,mode = "in")
+      find_roots=intersect(go_sub_roots,names(unlist(parent_nodes)))
+    }else{
+      parent_nodes=ego(reactome_ontology,order = gorder(reactome_ontology),x,mode = "in")
+      find_roots= intersect(reactome_roots,names(unlist(parent_nodes)))
+    }
+    if(length(find_roots)>1){
+      find_roots=paste0(unlist(find_roots),collapse = "#")
+    }
+    return(find_roots)
+  })
+  names(find_roots)=selected_pathways
+  return(find_roots)
+  
+}
+
+
+#' from_id2name
+#' 
+#' If you use the default pathway databases(GO Ontologyand REACTOME),this function can help you to get pathways names from pathways IDs.
+#' @param selected_pathways A list of GO and/or REACTOME pathways IDs. Each elmment is this list can be a single id or multi-ids seperated "#" 
+#' @return  A list of GO sub-root or REACTOME root names for provided pathways. 
+#' @keywords 
+#' @export
+#' @examples
+#' from_id2name((selected_pathways=list(c("GO:0032991#GO:0044425#GO:0044464"),"R-HSA-5357801")))
+from_id2name=function(selected_pathways){
+  go_ontology=readRDS(system.file("data", "human_go_ontology.rds", package = "GENEMABR"))
+  reactome_ontology=readRDS(system.file("data", "human_reactome_ontology.rds", package = "GENEMABR"))
+  
+  go_ontology_names=V(go_ontology)$pathway_names
+  names(go_ontology_names)=as_ids(V(go_ontology))
+  reactome_ontology_names=V(reactome_ontology)$pathway_names
+  names(reactome_ontology_names)=as_ids(V(reactome_ontology))
+  
+  names=lapply(selected_pathways, function(x){
+    if(grepl("#",x)){
+      if(grepl("GO",x)){
+        y=strsplit(x,split = "#")[[1]]
+        unname(go_ontology_names[y])
+      }else{
+        y=strsplit(x,split = "#")[[1]]
+        unname(reactome_ontology_names[y])
+      }
+    }else{
+      if(grepl("GO",x)){
+        unname(go_ontology_names[x])
+      }else{
+        unname(reactome_ontology_names[x])
+      }
+    }
+  })
+  names(names)=selected_pathways
+  return(names)
+}
+
+#' get_steps
+#' 
+#' If you use the default pathway databases(GO Ontologyand REACTOME),this function allows you to extract  the distances from ceatain pathways to  GO roots or REACTOME roots nodes.
+#' @param selected_pathways A vecor of GO and/or REACTOME pathways IDs. 
+#' @return  A list contains distances from pathways to GO root or REACTOME root nodes
+#' @keywords 
+#' @export
+#' @examples
+#' get_steps(selected_pathways=c("GO:0005834","R-HSA-111469"))
+get_steps=function(selected_pathways){
+  go_ontology=readRDS(system.file("data", "human_go_ontology.rds", package = "GENEMABR"))
+  go_roots=readRDS(system.file("data", "human_go_roots.rds", package = "GENEMABR"))
+  go_roots=unlist(go_roots)
+  reactome_ontology=readRDS(system.file("data", "human_reactome_ontology.rds", package = "GENEMABR"))
+  reactome_roots=readRDS(system.file("data", "human_reactome_roots.rds", package = "GENEMABR"))
+  
+  steps=lapply(selected_pathways,function(x){
+    if(grepl("GO",x)){
+      min(distances(go_ontology,v=go_roots,to=x))
+    }else{
+      min(distances(reactome_ontology,v=reactome_roots,to=x))
+    }
+  })
+  names(steps)=selected_pathways
+  return(steps)
+}
+
