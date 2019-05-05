a676a053cd668bbaf7b2a730a6e36d14d430aec4
Jitao David Zhang <jitao_david.zhang@roche.com>
2019-05-05 08:56:44 +0200

    use mydata now instead of data

--- R/supplementary_functions.R
+++ R/supplementary_functions.R
@@ -1,3 +1,13 @@
+#' Helper function to load file
+#' @param file .RData file to be loaded
+#' @return The object
+#' This function is temporarily set up to help the transition from .rds to .RData file
+mydata <- function(objName, packageName) {
+    nv <- new.env()
+    data(list=c(objName), package=packageName, envir = nv)
+    return(get(objName, envir = nv))
+}
+
 #' fisher_exact_test
 #'
 #' This function allows you to compute two sided fish exact pvalue of gene list for selected  pathways
@@ -19,7 +29,7 @@
 #' a=fisher_exact_test(selected_pathways=c("GO:0007250","GO:0008625"),gene_input=c("TRPC4AP","CDC37","TNIP1","IKBKB","NKIRAS2","NFKBIA","TIMM50","RELB","TNFAIP3","NFKBIB","HSPA1A","NFKBIE","SPAG9","NFKB2","ERLIN1","REL","TNIP2","TUBB6","MAP3K8"),gene_pathway_matrix="default")
 fisher_exact_test=function(selected_pathways,gene_input,gene_pathway_matrix="default"){
   if(gene_pathway_matrix=="default"){
-    gene_pathway_matrix=data("gene_pathway_matrix", package="GENEMABR")
+    gene_pathway_matrix=mydata("gene_pathway_matrix", package="GENEMABR")
   }
   all_genes=rownames(gene_pathway_matrix)
   module1_common_genes=intersect(all_genes,gene_input)
@@ -56,11 +66,11 @@ fisher_exact_test=function(selected_pathways,gene_input,gene_pathway_matrix="def
 #' @examples
 #' find_root_ids(selected_pathways=c("GO:0005834","R-HSA-111469"))
 find_root_ids=function(selected_pathways){
-  go_ontology=data("human_go_ontology", package="GENEMABR")
-  go_sub_roots=data("human_go_sub_roots", package="GENEMABR")
+  go_ontology=mydata("human_go_ontology", package="GENEMABR")
+  go_sub_roots=mydata("human_go_sub_roots", package="GENEMABR")
   go_sub_roots=unlist(go_sub_roots)
-  reactome_ontology=data("human_reactome_ontology", package="GENEMABR")
-  reactome_roots=data("human_reactome_roots", package="GENEMABR")
+  reactome_ontology=mydata("human_reactome_ontology", package="GENEMABR")
+  reactome_roots=mydata("human_reactome_roots", package="GENEMABR")
 
   find_roots=lapply(selected_pathways, function(x){
     if(grepl("GO",x)){
@@ -86,12 +96,13 @@ find_root_ids=function(selected_pathways){
 #' If you use the default pathway databases(GO Ontologyand REACTOME),this function can help you to get pathways names from pathways IDs.
 #' @param selected_pathways A list of GO and/or REACTOME pathways IDs. Each elmment is this list can be a single id or multi-ids seperated "#"
 #' @return  A list of GO sub-root or REACTOME root names for provided pathways.
+#' @importFrom igraph V
 #' @export
 #' @examples
 #' from_id2name((selected_pathways=list(c("GO:0032991#GO:0044425#GO:0044464"),"R-HSA-5357801")))
 from_id2name=function(selected_pathways){
-  go_ontology=data("human_go_ontology", package="GENEMABR")
-  reactome_ontology=data("human_reactome_ontology", package="GENEMABR")
+  go_ontology=mydata("human_go_ontology", package="GENEMABR")
+  reactome_ontology=mydata("human_reactome_ontology", package="GENEMABR")
 
   go_ontology_names=V(go_ontology)$pathway_names
   names(go_ontology_names)=as_ids(V(go_ontology))
@@ -128,11 +139,11 @@ from_id2name=function(selected_pathways){
 #' @examples
 #' get_steps(selected_pathways=c("GO:0005834","R-HSA-111469"))
 get_steps=function(selected_pathways){
-  go_ontology=data("human_go_ontology", package="GENEMABR")
-  go_roots=data("human_go_roots", package = "GENEMABR")
+  go_ontology=mydata("human_go_ontology", package="GENEMABR")
+  go_roots=mydata("human_go_roots", package = "GENEMABR")
   go_roots=unlist(go_roots)
-  reactome_ontology=data("human_reactome_ontology", package="GENEMABR")
-  reactome_roots=data("human_reactome_roots", package="GENEMABR")
+  reactome_ontology=mydata("human_reactome_ontology", package="GENEMABR")
+  reactome_roots=mydata("human_reactome_roots", package="GENEMABR")
 
   steps=lapply(selected_pathways,function(x){
     if(grepl("GO",x)){
