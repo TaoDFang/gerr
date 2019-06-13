823dc7cd3c10c875277fd8020d89dc4a516208e9
Tao Fang <talonvonfang@gmail.com>
2019-06-13 22:17:36 +0100

    update verbose flag

--- R/regression_selected_pathways_funcs.R
+++ R/regression_selected_pathways_funcs.R
@@ -54,7 +54,7 @@ regression_selected_pathways=function(gene_input,gene_pathway_matrix=NULL,alpha=
   module_labels[module_common_genes] <- 1
   
   if(length(module_common_genes)<=1) {
-    if(!verbose){
+    if(verbose){
       warning("Not enough genes in the set of genes of interest. NULL is returned.\n")
     }
     return(NULL)
@@ -67,7 +67,7 @@ regression_selected_pathways=function(gene_input,gene_pathway_matrix=NULL,alpha=
                   ...))
 
   if(class(cvfit)=="try-error") {
-    if(!verbose){
+    if(verbose){
       warning("cv.glmnet failed. The error and the model input is returned for debugging purposes.\n")
     }
     res <- list(model=cvfit,
@@ -107,7 +107,7 @@ regression_selected_pathways=function(gene_input,gene_pathway_matrix=NULL,alpha=
                 y=module_labels)
 
   } else {
-    if(!verbose){
+    if(verbose){
       warning("No selected gene-sets with the given parameter set. NULL is returned.\n")
     }
     
